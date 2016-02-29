0221b152dc48e87125a7526c23f835792e549347
rakesh635 <rakesh635@gmail.com>
2016-02-29 15:00:14 +0530

    Create MultiplyTest.java

---
+++ MultiplyTest.java
@@ -0,0 +1,17 @@
+
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MultiplyTest {
+
+	@Test
+	public void testMultiply() {
+		Junits test = new Junits();
+		int result = test.multiply(3,5);
+		assertEquals(15,result);
+	}
+
+}
