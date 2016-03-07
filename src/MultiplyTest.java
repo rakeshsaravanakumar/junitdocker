f22878d013f0fb77e953597749c21aa7d01136aa
rakesh635 <rakesh635@gmail.com>
2016-03-07 16:01:00 +0530

    Update MultiplyTest.java

--- src/MultiplyTest.java
+++ src/MultiplyTest.java
@@ -10,7 +10,7 @@ public class MultiplyTest {
 	public void testMultiply() {
 		Junits test = new Junits();
 		int result = test.multiply(3,5);
-		assertEquals(16,result);
+		assertEquals(15,result);
 	}
 
 }
