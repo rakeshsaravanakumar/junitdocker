d3eb860eafed5586c48e158f625655e2155a2702
rakesh635 <rakesh635@gmail.com>
2016-03-07 17:16:05 +0530

    Update AllTests.java

--- src/testpackage/AllTests.java
+++ src/testpackage/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ ConcateTest.class, MultiplyTest.class })
+@SuiteClasses({ concatenatetest.class, multiplytest.class })
 public class AllTests {
 
 }
