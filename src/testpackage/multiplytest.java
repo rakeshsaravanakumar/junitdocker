d1ef5611f07bab4a0d3fa25fda2c99d8ff654045
rakesh635 <rakesh635@gmail.com>
2016-03-07 17:14:50 +0530

    Create multiplytest.java

---
+++ src/testpackage/multiplytest.java
@@ -0,0 +1,17 @@
+package testpackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class multiplytest {
+
+	@Test
+	public void testMultiply() {
+		rakeshclass test = new rakeshclass();
+		int result = test.multiply(3, 5);
+		assertEquals(15, result);
+	}
+
+}
